b12fa5e2c37956699e92b80252cdaed788ab49a8
Shadman Ali <shadman.ali@live.co.uk>
2023-12-25 19:36:57 +0530

    refactor

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv"
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json()); 
